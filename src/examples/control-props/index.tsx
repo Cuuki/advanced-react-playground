97a19a943b5f298efcd4aae34e38f28f4b3e3a0a
Patrick Söllner <mail@patrick-soellner.de>
2020-12-28 17:22:26 +0100

    add controls props toggle example

--- src/examples/control-props/index.tsx
+++ src/examples/control-props/index.tsx
@@ -1,5 +1,59 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
+import Switch from '../../components/Switch';
 
-const ControlPropsExample: React.FC = () => null;
+interface ToggleProps {
+  on?: boolean;
+  onToggle: (on: boolean) => void;
+}
+
+const Toggle: React.FC<ToggleProps> = ({on, onToggle}) => {
+  const [internalOn, setInternalOn] = useState<boolean>(false);
+
+  const toggle = useCallback(() => {
+    if (on !== undefined) {
+      onToggle(!on);
+
+      return;
+    }
+
+    setInternalOn((prevOn) => {
+      const newOn = !prevOn;
+
+      onToggle(newOn);
+
+      return newOn;
+    });
+  }, [on, onToggle]);
+
+  return <Switch on={on !== undefined ? on : internalOn} onClick={toggle} />;
+};
+
+const ControlPropsExample: React.FC = () => {
+  const [on, setOn] = useState<boolean>(false);
+  const [inputValue, setInputValue] = useState<string>('off');
+
+  const handleToggle = useCallback((on) => {
+    setOn(on);
+    setInputValue(on ? 'on' : 'off');
+  }, []);
+
+  const handleChange = useCallback(({target: {value}}) => {
+    if (value === 'on') {
+      setOn(true);
+    } else if (value === 'off') {
+      setOn(false);
+    }
+
+    setInputValue(value);
+  }, []);
+
+  return (
+    <div>
+      <input value={inputValue} onChange={handleChange} />
+      <Toggle on={on} onToggle={handleToggle} />
+      <Toggle onToggle={handleToggle} />
+    </div>
+  );
+};
 
 export default ControlPropsExample;
